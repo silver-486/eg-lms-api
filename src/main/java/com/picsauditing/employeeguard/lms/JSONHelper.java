5bca2beab7f4ce2848db4be2b511e9b69afbe230
Pablo Ducuron <pducuron@picsauditing.com>
2014-11-10 13:42:53 -0800

    - json helper

--- src/main/java/com/picsauditing/employeeguard/lms/JSONHelper.java
+++ src/main/java/com/picsauditing/employeeguard/lms/JSONHelper.java
@@ -5,19 +5,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
 import java.lang.reflect.Modifier;
+import java.util.List;
 
 public class JSONHelper {
 
 
-  public static String toJSONJackson(Object message) throws JsonProcessingException {
+  public static String toJSON(Object message) throws JsonProcessingException {
     ObjectMapper mapper = new ObjectMapper();
     mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
     return mapper.writeValueAsString(message);
   }
 
-  public static String toJSON(Object obj) {
+  public static String toJSONGson(Object obj) {
     return new GsonBuilder().setPrettyPrinting().excludeFieldsWithModifiers(Modifier.STATIC, Modifier.TRANSIENT).create().toJson(obj);
+  }
+
+  public static List<Integer> toIdList(String json) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    return mapper.readValue(json, List.class);
   }
 
 
---
+++ src/test/java/com/picsauditing/employeeguard/lms/JSONHelperTest.java
@@ -0,0 +1,21 @@
+package com.picsauditing.employeeguard.lms;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class JSONHelperTest {
+
+  @Test
+  public void testToIdList() throws Exception {
+    List<Integer> expected = Arrays.asList(1,2,3,4,5);
+    String json = "["+StringUtils.join(expected.toArray(), ',')+"]";
+    System.out.println(" json: "+ json);
+    List<Integer> actual = JSONHelper.toIdList(json);
+    assertEquals(expected, actual);
+  }
+}
