ce866648a73212b88a637ef5e3f49c99dffd8fd4
Pablo Ducuron <pducuron@picsauditing.com>
2014-11-11 23:06:34 -0800

    - Hooked up all the commands to see JSON output

--- src/main/java/com/picsauditing/employeeguard/lms/controller/LmsController.java
+++ src/main/java/com/picsauditing/employeeguard/lms/controller/LmsController.java
@@ -13,8 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.picsauditing.employeeguard.lms.model.api.Command.ADD_USER;
-import static com.picsauditing.employeeguard.lms.model.api.Command.UPDATE_USER;
+import static com.picsauditing.employeeguard.lms.model.api.Command.*;
 
 @RestController
 class LmsController {
@@ -34,7 +33,23 @@ class LmsController {
   public ResponseEntity<MessageResponse> testLmsApi() throws Exception {
 
 //    Message message = mocker.mockerMessageTest();
-    Message message = mockRequestService.mockRequest(ADD_USER, UPDATE_USER);
+    Message message = mockRequestService.mockRequest(
+      ADD_USER,
+      UPDATE_USER,
+      DEACTIVATE_USER,
+      DELETE_USER,
+      GET_USERS_BY_IDS,
+      GET_ALL_USER_IDS,
+      GET_COURSE_LINK,
+      ASSIGN_EMPLOYEE_TO_TRAINING,
+      GET_LAUNCH_LINK,
+      ADD_PICS_ACCOUNT,
+      UPDATE_PICS_ACCOUNT,
+      DEACTIVATE_PICS_ACCOUNT,
+      DELETE_PICS_ACCOUNT,
+      FIND_PICS_ACCOUNT_BY_IDS,
+      GET_ALL_PICS_ACCOUNT_IDS
+    );
 
     MessageResponse messageResponse = apiService.sendMessage(message);
 
